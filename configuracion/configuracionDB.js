718985177336f95fc939267cd62b5687695cb640
Jonnthan3visan <joptrabajo@hotmail.com>
2024-11-22 10:43:10 -0300

    configuracion de DB

--- configuracion/configuracionDB.js
+++ configuracion/configuracionDB.js
@@ -1,22 +1,13 @@
-import mysql from 'mysql2';
+import { createPool } from 'mysql2/promise';
 import dotenv from 'dotenv';
-
-dotenv.config();
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+dotenv.config()
+const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 1,
+    queueLimit: 0
 });
-
-// Verificar la conexión
-connection.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-    return;
-  }
-  console.log('Conexión a la base de datos MySQL exitosa!');
-});
-
-export default connection;
+export default pool;
