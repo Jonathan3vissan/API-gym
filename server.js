19d523463c277dbc430e5064d39a73e152923a5e
Jonnthan3visan <joptrabajo@hotmail.com>
2024-11-23 20:31:23 -0300

    agregado modulo cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express, { json } from 'express';
 import usaRutas from './rutas/rutasUsuario.js';
+import cors from 'cors'; // Importar CORS
 
 const server = express();
 
+// Habilitar CORS para todas las solicitudes
+server.use(cors());
 // Middleware para manejar JSON
 server.use(json());
 
